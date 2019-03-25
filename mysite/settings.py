c43af9f699d720f68961111f34ade60bf68df7f0
Ilya Loginov <ilya2707004@yandex.ru>
2019-03-25 22:09:43 +0500

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'md%5jch7fo6_&^&hdrvjyikl#84210@g4evm@d1l_-25ccbmx4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'kagent.herokuapp.com']
 
 
 # Application definition
