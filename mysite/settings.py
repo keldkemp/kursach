d1122b608cc6225f3584baa6c830898a9a991538
Ilya Loginov <ilya2707004@yandex.ru>
2019-03-25 22:28:31 +0500

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'md%5jch7fo6_&^&hdrvjyikl#84210@g4evm@d1l_-25ccbmx4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'kagent.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'kagent.herokuapp.com']
 
 
 # Application definition
